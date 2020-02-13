176a1b99faaa1aca1796bc58ce80e012e5adc8be
liberjul <liberjul@msu.edu>
2020-02-13 13:50:07 -0500

    Data sim matrix made

--- sim_data.R
+++ sim_data.R
@@ -2,3 +2,22 @@ n.areas <- 2
 n.species <- 2
 n.samples <- 20
 n <- n.areas * n.species * n.samples
+
+areas <-gl(n=n.areas,k=n/n.areas)
+areas
+
+species <-gl(n=n.species,k=n.samples,length=n)
+species
+
+depth <- runif(n=n, min=0, max=5)
+depth_st <- (depth - mean(depth))/sd(depth)
+
+hist(depth_st)
+
+size <- rgamma(n = n,shape = 3, rate = 0.1)
+size_st <- (size-mean(size))/sd(size)
+
+hist(size_st)
+
+Xmat = model.matrix(~areas*species+depth_st+size_st)
+Xmat
